5308c61a0524ad615c609cd2ddb996f74d64478b
Jieyuhua9193 <1500690094@qq.com>
2020-09-01 09:25:58 +0800

    less update and layout-side-menu 10%

--- client/src/layout/components/SideMenu.vue
+++ client/src/layout/components/SideMenu.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="side-menu-wrapper">
-    <div class="side-menu-class-info">
-      
+    <div class="class-info">
+      <img
+        src="http://qiniu.jieyuhua.top/blog/library/13.jpg" 
+        alt="班级头像"
+        class="avatar">
+      <div class="info">
+        <p class="class-name">软件技术171</p>
+        <p>班长</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-export default {}
+import { ref,onMounted } from 'vue'
+
+export default {
+}
 </script>
